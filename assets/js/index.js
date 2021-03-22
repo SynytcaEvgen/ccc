4ee1e600b189f0a431163f935f130043d2ec8346
Yevhen Synytsia <yevhensynytsia@tutanota.com>
2021-03-22 18:19:47 +0200

    day_commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -32,7 +32,11 @@ function init() {
            slidesPerView: 2.08,
         },
          480: {
-           slidesPerView: 3,
+           slidesPerView: 2.2,
+           spaceBetween: 4,
+        },
+         600: {
+           slidesPerView: 2.8,
            spaceBetween: 4,
          },
          768: {
@@ -48,6 +52,28 @@ function init() {
 		  },
     });
   /*Slider swiper prod-slider end*/
+  function accordionMover() {
+    let acc = document.querySelectorAll(".accordion-btn");
+    for (let i = 0; i < acc.length; i++) {
+      acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        let panel = acc[i].parentElement.previousElementSibling;
+         if (panel.style.maxHeight&&panel.classList.contains('accordion')) {
+           panel.style.maxHeight = null;
+         } else {
+           panel.style.maxHeight = panel.scrollHeight + "px";
+         } 
+       });
+    }; 
+  };
+  function addRemoveClass(elem, add) {
+    if ($(window).width() <= 900) {
+      $(elem).addClass(add);
+    } else {
+      $(elem).removeClass(add);
+    };
+  }
+
 
   $('.features_items .search').click(function () {
         if ($(window).width() >= 900){
@@ -90,5 +116,13 @@ function init() {
   $('.promo_mob svg').click(function () {
     $(this).parent().css('height', '0');
   });
-
+  addRemoveClass('.info-frame .info-content-wrapper>.title','accordion-btn');
+  addRemoveClass('section.info .info-menu-wrapper','accordion');
+  $(window).resize(function () {
+    addRemoveClass('.info-frame .info-content-wrapper>.title','accordion-btn');
+    addRemoveClass('section.info .container .info-frame .info-menu-wrapper', 'accordion');
+    accordionMover();
+    
+  });
+  accordionMover();
 };
